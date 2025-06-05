5b7ca06bed7aa0bad1fc10dd2aa13015f16996a3
Barbara Moura <110795153+Barbmoura@users.noreply.github.com>
2025-06-05 14:19:16 -0300

    Desafio index procedure

--- desafio_index_procedure.sql
+++ desafio_index_procedure.sql
@@ -6,9 +6,6 @@ CREATE INDEX idx_categoria ON Produto(Categoria) USING BTREE; -- Busca por categ
 CREATE INDEX idx_status ON Pedido(StatusPedido) USING BTREE; -- Busca por status do pedido. Exemplo: Enviado
 
 -- Parte 2: Utilização de procedures para manipulação de dados em Banco de Dados 
--- Criar uma procedure que possua as instruções de inserção, remoção e atualização de dados no banco de dados. As instruções devem estar dentro de estruturas condicionais (como CASE ou IF).
--- Além das variáveis de recebimento das informações, a procedure deverá possuir uma variável de controle. Essa variável de controle irá determinar a ação a ser executada. Ex: opção 1 – select, 2 – update, 3 – delete.  
-
 CREATE DATABASE Usuario;
 USE Usuario;
 
